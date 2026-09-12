0d67b1e2154f76e668995394f52ed3a8496b2c67
Miguelrenearana <miguelreneas@gmail.com>
2026-09-12 18:10:53 -0400

    chore: remove entire project, keep git history

--- src/FashionStore.Core/Entities/Role.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace FashionStore.Core.Entities;
-
-public class Role : IdentityRole<int>
-{
-    public string? Description { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
